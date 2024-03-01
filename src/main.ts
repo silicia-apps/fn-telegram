b34e33fca453cb144e22b5fa2874fb4e0275256a
Francesco Bozzotta <39646929+silicia-apps@users.noreply.github.com>
2024-03-01 09:29:11 +0000

    xxx

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,8 @@ type Context = {
 
 export default async ({ req, res, log, error }: Context) => {
 
-error(req.query.postazione);
-log(res.query.secret);
+error(encodeURIComponent(req.query.postazione));
+log(req.query.secret);
 
 const bot = new Telegraf('6874400408:AAGq6X_RRI_A6J9v6PfMSdNMOd55BldktJI');
 
